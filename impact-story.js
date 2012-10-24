ee33fbcca4bfa88ce49de088b13e2a830c6bd2a6
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-10-23 20:28:37 -0700

    Reorganizing things into methods

--- impact-story.js
+++ impact-story.js
@@ -1,7 +1,31 @@
-$.ajax({
-  url: "http://api.total-impact.org/item/doi/10.1371/journal.pcbi.1000361",
-  type: 'POST',
-  dataType: 'json'
-}).done(function (data) {
-  alert(data);
-});
+var impactStory = {};
+
+impactStory.fetchTIID = function(doi, callback) {
+  $.ajax({
+    url: "http://api.total-impact.org/item/doi/" + doi,
+    type: 'POST',
+    dataType: 'json'
+  }).done(function (data) {
+    callback(data);
+  });
+}
+
+// Aliases is a list of key-value pairs. For example: [['pmid','12345'],['doi','10.1371/journal.pbio.1000056']]
+// Title of collection
+// Callback to be called when the collection is done loading
+impactStory.createCollection(aliases, title, callback) {
+  var data = {
+    'aliases' : aliases,
+    'title': title,
+  };
+  
+  $.ajax({
+    url: "http://api.total-impact.org/collection",
+    type: 'POST',
+    dataType: 'json',
+    data: $.encodeJSON(data)
+  }).done(function (data) {
+    callback(data);
+  });
+}
+
