2857f6aeca6d75ccac68b0bc64163e35163a9963
dweebit <brakit@gmail.com>
2012-11-03 13:40:33 -0700

    WIP

--- bookmarklet.js
+++ bookmarklet.js
@@ -42,7 +42,7 @@ var parse = function() {
     references.each(function(index) {
 	var dd = $(this).find('.rprtid dd')[0];
 	//con.append(dd)
-	ref_objs[dd] = $(this)
+	ref_objs[dd] = $(this);
     });
     con.append(ref_objs);
     //con.append(references);
