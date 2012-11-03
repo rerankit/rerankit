7a4acdc5c1042ded7391df91fe4f2f4344f61b5a
Jason Priem <jp@jasonpriem.org>
2012-11-03 14:54:48 -0700

    bugfixes in url strings.

--- impact-story.js
+++ impact-story.js
@@ -59,7 +59,7 @@ impactStory.getCollection = function(collection, callback, error, conf) {
   }
 
     $.ajax({
-               url: "http://api.total-impact.org/collection"+collectionId+'?api_key='+test,
+               url: "http://api.total-impact.org/collection/"+collectionId+'?api_key='+"test",
                type: "GET",
                dataType: "json",
                contentType: "application/json; charset=utf-8",
@@ -75,7 +75,7 @@ impactStory.getCollection = function(collection, callback, error, conf) {
                        console.log("done with updating")
 
                        // run the success callback
-                       successCallback(data)
+                       callback(data)
 
                        return false;
                    }
