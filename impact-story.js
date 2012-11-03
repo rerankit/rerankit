fef101ce7d16f7de5e90d83547d622d555986857
Jason Priem <jp@jasonpriem.org>
2012-11-03 13:03:37 -0700

    impactStory.createCollection() works.

--- impact-story.js
+++ impact-story.js
@@ -16,8 +16,8 @@ impactStory.fetchTIID = function(doi, callback) {
 // aliases: list of key-value pairs. For example: [['pmid','12345'],['doi','10.1371/journal.pbio.1000056']]
 // title: Title of collection
 // callback: Callback to be called when the collection is done loading. function(data)
-impactStory.createCollection(aliases, title, callback) {
-  var data = {
+impactStory.createCollection = function(aliases, title, callback) {
+  var postData = {
     'aliases' : aliases,
     'title': title,
   };
@@ -26,9 +26,10 @@ impactStory.createCollection(aliases, title, callback) {
     url: "http://api.total-impact.org/collection",
     type: 'POST',
     dataType: 'json',
-    data: $.encodeJSON(data)
-  }).done(function (data) {
-    callback(data);
+    contentType:"application/json; charset=utf-8",
+    data: JSON.stringify(postData)
+  }).done(function (returnedData) {
+    callback(returnedData);
   });
 }
 
