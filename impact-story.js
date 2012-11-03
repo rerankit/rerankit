e85e4410464925da5d46d3d8d4b984a0c51db0cb
Jason Priem <jp@jasonpriem.org>
2012-11-03 14:24:53 -0700

    fixing callback soup.

--- impact-story.js
+++ impact-story.js
@@ -4,42 +4,20 @@ var impactStory = {};
 // doi: DOI
 // callback: callback function to be called. function(tiid)
 impactStory.fetchTIID = function(doi, callback, error) {
-  $.ajax({
-    url: "http://api.total-impact.org/item/doi/" + doi,
-    type: 'POST',
-    dataType: 'json'
-  }).done(function (data) {
-    callback(data);
-  }).error(function (error) {
-    if (error) {
-      callback(error);
-    }
-  });
+    $.ajax({
+               url: "http://api.total-impact.org/item/doi/" + doi,
+               type: 'POST',
+               dataType: 'json'
+           }).done(function (data) {
+                       callback(data);
+                   }).error(function (error) {
+                                if (error) {
+                                    callback(error);
+                                }
+                            });
 }
 
-// aliases: list of key-value pairs. For example: [['pmid','12345'],['doi','10.1371/journal.pbio.1000056']]
-// title: Title of collection
-// callback: Callback to be called when the collection is done loading. function(data)
-impactStory.createCollection = function(aliases, title, callback, error) {
-  var postData = {
-    'aliases' : aliases,
-    'title': title,
-  };
-  
-  $.ajax({
-    url: "http://api.total-impact.org/collection",
-    type: 'POST',
-    dataType: 'json',
-    contentType:"application/json; charset=utf-8",
-    data: JSON.stringify(postData)
-  }).done(function (returnedData) {
-    callback(returnedData);
-  }).error(function (error) {
-    if (error) {
-      callback(error);
-    }
-  });
-}
+
 
 
 // conf: {
@@ -48,24 +26,24 @@ impactStory.createCollection = function(aliases, title, callback, error) {
 //  interval: 1000
 //  partial: function(data)
 //}
-impactStory.getCollection = function(collection, callback, error, conf) {
+impactStory.getCollection = function(collection, successCallback, error, conf) {
 
-  // Get the collection ID. Can pass either a string, a "create-collection" meta-object, or a collection object
-  var collectionId;
-  if (typeof collection == 'string') {
-    collectionId = collection;
-  }
-  if (typeof collection == 'object') {
-    if (collection.hasOwnProperty('collection')) {
-      collectionId = collection.collection._id;
+    // Get the collection ID. Can pass either a string, a "create-collection" meta-object, or a collection object
+    var collectionId;
+    if (typeof collection == 'string') {
+        collectionId = collection;
     }
-    else {
-      collectionId = collection._id;
+    if (typeof collection == 'object') {
+        if (collection.hasOwnProperty('collection')) {
+            collectionId = collection.collection._id;
+        }
+        else {
+            collectionId = collection._id;
+        }
     }
-  }
 
     $.ajax({
-               url: "http://api.total-impact.org/collection"+collectionId+'?api_key='+test,
+               url: "http://api.total-impact.org/collection/"+collectionId+'?api_key='+"test",
                type: "GET",
                dataType: "json",
                contentType: "application/json; charset=utf-8",
@@ -80,11 +58,41 @@ impactStory.getCollection = function(collection, callback, error, conf) {
                        console.log("done with updating")
 
                        // run the success callback
-                       callback(data)
+                       successCallback(data)
 
                        return false;
                    }
                }
            });
 
+}
+
+
+// aliases: list of key-value pairs. For example: [['pmid','12345'],['doi','10.1371/journal.pbio.1000056']]
+// title: Title of collection
+// callback: successCallback to be called when the collection is done loading. function(data)
+impactStory.createCollection = function(aliases, title, successCallback, error) {
+    var postData = {
+        'aliases' : aliases,
+        'title': title,
+    };
+
+    $.ajax({
+               url: "http://api.total-impact.org/collection",
+               type: 'POST',
+               dataType: 'json',
+               contentType:"application/json; charset=utf-8",
+               data: JSON.stringify(postData)
+           }).done(function (returnedData) {
+                       var collection = returnedData.collection._id
+                       var callback = impactStory.getCollection
+                       var conf = {}
+
+                       impactStory.getCollection(collection, successCallback, error, conf);
+                   })
+            .error(function (error) {
+                                if (error) {
+                                    callback(error);
+                                }
+                            });
 }
