4a688cb10197304a2caa3dfe08cf3ae7aa7ac5b3
dweebit <brakit@gmail.com>
2012-11-03 12:06:07 -0700

    WIP

--- bookmarklet.js
+++ bookmarklet.js
@@ -20,11 +20,11 @@ window.bookmarklet({
     js  : [],    
 //	jqpath : 'myCustomjQueryPath.js', <-- option to include your own path to jquery
     ready : function(){
-	console.log("Am i working?");
-		// The meat of your jQuery code goes here
-		$("body").html("Hello World");
+	console.log("DEBUG: start");
+	// The meat of your jQuery code goes here
+	parse();
  
-   	    }
+    }
 })
  
 function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}$.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>").attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js";(function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;
@@ -32,3 +32,7 @@ c.onload=function(){e(a.css);d(a.js)};
 document.body.appendChild(c)
 })(a.jqpath)};
 
+var parse = function() {
+    var references = $('#references').next('ol');
+    console.log(references);
+};
